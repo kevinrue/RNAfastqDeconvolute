61e5c5c0aafe3a8efe402fd764c07dddd8a51644
krue <kevin.rue@ucdconnect.ie>
2014-03-07 18:50:49 +0000

    Partial implementation of #2

--- src/Processing_paired_reads.py
+++ src/Processing_paired_reads.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
-"""docstring:
-This serves as a long usage message.
+"""
+NAME
+    Processing_paired_reads.py - Deconvolute, trims and filters fastq files
 
-This script is the main script for the processing of paired reads in fastq
+SNYOPSIS
+    Processing_paired_reads --forward raw1.fastq.gz --reverse raw2.fastq.gz --index indices.txt ...
+
+DESCRIPTION
+    Parses two fastq files containing paired reads from a sequencing lane, deconvolutes the reads according to
+    a barcode/sample mapping file, filters out reads contaminated with adapter sequence provided by the user, filters
+    out reads of poor quality based on user-defined thresholds, and trims the reads according to user instructions.
+
+    -a, --adapter file
+        TAB-separated file containing three columns. First, the adapter sequence as expected in the read; Second, an
+        identifier unique to the adapter sequence; Third, one of "forward" or "reverse" specifying the mate in which
+        to look for the adapter sequence.
+
+This is Contained in variable named __doc__
 
 This code was based on an example found at https://www.artima.com/weblogs/viewpost.jsp?thread=4829
 by Guido van Rossum (creator of Python).
 """
 
-__author__ = 'David Magee, Carolina Correia, and Kevin Rue'
+__author__ = 'David Magee, Carolina Correia, and Kevin Rue-Albrecht'
 __copyright__ = "Copyright 2014, GPLv2"
 
 # Module sys allows to interact with the operating system
@@ -33,7 +47,7 @@ def main():
         sys.exit(2)
     # process options
     for o, a in opts:
-          # For training purpose: shows all the options identified
+        # For training purpose: shows all the options identified
         print(o, a)
         # if the "help" option is given, prints the docstring (above)
         if o in ("-h", "--help"):
