e87d6c2c268d89d71c85e56be2dd467f6afdb031
krue <kevin.rue@ucdconnect.ie>
2014-03-08 11:11:12 +0000

    Fixes #2
    (almost) Complete docstring. I will open another issue about the
    adapter_param option. We need to decide the format of that particular
    option before writing the coresponding docstring.

--- src/Processing_paired_reads.py
+++ src/Processing_paired_reads.py
@@ -4,23 +4,55 @@
 NAME
     Processing_paired_reads.py - Deconvolute, trims and filters fastq files
 
-SNYOPSIS
-    Processing_paired_reads --forward raw1.fastq.gz --reverse raw2.fastq.gz --index indices.txt ...
+SYNOPSIS
+    Processing_paired_reads.py --forward raw1.fastq.gz --reverse raw2.fastq.gz --index indices.txt ...
 
 DESCRIPTION
     Parses two fastq files containing paired reads from a sequencing lane, deconvolutes the reads according to
     a barcode/sample mapping file, filters out reads contaminated with adapter sequence provided by the user, filters
     out reads of poor quality based on user-defined thresholds, and trims the reads according to user instructions.
 
-    -a, --adapter file
+    -f, --forward file.fastq.gz
+        GZIP-compressed fastq file containing the forward reads.
+
+    -r, --reverse file.fastq.gz
+        GZIP-compressed fastq file containing the reverse reads.
+
+    -i, --index file.txt
+        TAB-separated file containing two columns. First, the barcode as expected in the read; Second, an identifier
+        unique to the corresponding sample (e.g. animal_time_treatment).
+
+    -t, --trim STRING
+        The trimming parameters, specific in the format "5end_INTEGER1_3end_INTEGER2". INTEGER1 bases will be trimmed
+        off the 5' end and vice versa.
+
+    -a, --adapter file.txt
         TAB-separated file containing three columns. First, the adapter sequence as expected in the read; Second, an
         identifier unique to the adapter sequence; Third, one of "forward" or "reverse" specifying the mate in which
         to look for the adapter sequence.
 
-This is Contained in variable named __doc__
+    -A, --adapter_param STRING
+        (Format to be defined, depending on how we decide to scan the reads for the adapter sequences. For instance,
+        Nick required a STRING "I3S3D0" because he used the Perl module StringApprox which requires exactly this format
+        to do pattern matching. All Nick had to do is forward the STRING to the StringApprox module.)
 
-This code was based on an example found at https://www.artima.com/weblogs/viewpost.jsp?thread=4829
+    -I, -illumina_version DECIMAL
+        Version of the Illumina Genome Analyser pipeline used to generate the fastq files. This will affect the
+        conversion of the quality score from ASCII to PHRED using an offset of 33 or 64.
+
+    -p, --percent _thresh FLOAT
+        Maximal percentage of the read length allowed below the specified PHRED threshold (see -P, --phred). Reads with
+        a larger observed percentage will be filtered out due to "poor overall quality".
+
+    -P, --phred INTEGER
+        Threshold used to define sequenced nucleotides as "poor quality" (see -p, percent_thresh).
+
+
+The main function was based on a template posted at https://www.artima.com/weblogs/viewpost.jsp?thread=4829
 by Guido van Rossum (creator of Python).
+
+The overall deconvolution procedure programmed here was based on the original Perl version programmed by Nicolas
+Nalpas and Stephen Park (Animal Genomics Lab, UCD, Dublin, Ireland).
 """
 
 __author__ = 'David Magee, Carolina Correia, and Kevin Rue-Albrecht'
@@ -37,7 +69,8 @@ import getopt
 def main():
     # parse command line options
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hf:r:i:I:t:a:A:p:P:',
+        # saves the expected option/argument pairs in opts, and the remaining arguments in args
+        opts, args = getopt.getopt(sys.argv[1:], 'hf:r:i:t:a:A:I:p:P:',
                                    ["help", "forward=", "reverse=", "index=", "trim=", "adapter=",
                                     "adapter_param=", "illumina_version=", "percent_thresh=",
                                     "phred="])
