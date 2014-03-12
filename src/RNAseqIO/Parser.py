57abfe4c9a42013ce8ea053cd31330487d4bfd08
David Magee <david.magee@ucd.ie>
2014-03-12 18:01:13 +0000

    Added new descriptions to code
    Have provided explanations for the Parser and Read modules.
    Corrected typo in variable name.

--- src/RNAseqIO/Parser.py
+++ src/RNAseqIO/Parser.py
@@ -42,9 +42,12 @@ class FastqgzParser:
         """
         header_line = self.file_handler.readline().strip()
         sequence_line = self.file_handler.readline().strip()
-        separator_lne = self.file_handler.readline().strip()
+        separator_line = self.file_handler.readline().strip()
         quality_line = self.file_handler.readline().strip()
-        return Read.Read(header_line, sequence_line, separator_lne, quality_line)
+        return Read.Read(header_line, sequence_line, separator_line, quality_line)
+    # The first listed Read is a module. The second listed read is an objectClass type Read. This code takes each of
+    # the four variables (header_line, sequence_line, separator_line, quality_line) and inserts them into one single
+    # variable.
 
     def close(self):
         """Closes the file stream.
