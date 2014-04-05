59db7ef1f246a713af65d8bdef02fde3ae09a9b0
Tal Einat <taleinat@gmail.com>
2014-04-05 14:02:18 +0300

    removed the second approximate match check - no longer necessary

--- src/SeqDataTypes.py
+++ src/SeqDataTypes.py
@@ -74,54 +74,31 @@ class Read:
 
     def define_adapter_presence_substitutions_only(self, adapter, max_substitutions):
         """Sets the adapter_absent attribute according to whether a match is found with a number of substitutions
-        less or equal to
-        edit_threshold.
+        less or equal to edit_threshold.
 
         :rtype : None
-        Args:
-            self, adapter, edit_threshold
-
-        Returns:
-            None
-
         Args:
             self, adapter, max_substitutions
 
         Returns:
             None
         """
-        # Solves the straightforward case where the adapter is exactly present in the read
+        # Solves the straightforward case where the adapter is exactly present
+        # in the read
         if adapter in self.sequence_line:
             self.adapter_present = True
             return
-        # Otherwise, look for an approximate match of the adapter, less than max_substitutions different
-        # First do a preliminary (hopefully faster) check whether the adapter is present as an approximate match in
-        # the read sequence within a Levenshtein distance equal to the allowed number of substitutions
-        # Two sequences less than N substitutions apart are automatically less than a N edits apart (the reciprocal
-        # is not true). If the Levenshtein code is faster, then it will save a lot of time pre-filtering adapters
-        # within the Levenshtein distance, which are much more likely to contain adapters. The slower code checking
-        # the mismatch distance will then be called on these pre-filtered reads to confirm whether it is an actual
-        # substituted match or if the Levensthein match involved insertions and deletions
-        # If 1 or more approximate matches of the adapter were found within a Levenshtein distance of max_substitutions
-        if has_near_match_substitutions_ngrams(adapter, self.sequence_line, max_substitutions):
-            # scan the read sequence for a potential substituted match
-            # for each subsequence of the sequence_line of length identical to the adapter (last starting position is
-            # length of the adapter before the end of the read)
-            for index in range(len(self.sequence_line) - len(adapter) + 1):
-                # check if the adapter is less than max_substitutions away from the subsequence
-                result = ApproxMatch.approx_substitute(adapter, self.sequence_line[index:index + len(adapter)],
-                                                       max_substitutions)
-                # If a match was found (result is TRUE)
-                if result:
-                    # set the adapter presence to True (and stop the function here)
-                    self.adapter_present = True
-                    return
-                    # If no substring of sequence_line approximately matches the adapter, the function will eventually
-                    # leave the
-                    # loop above.
-                    # The simple fact of arriving here proves that no match was found, therefore leave the adapter
-                    # preence to False
-                    # and simply leave the function
+        # Otherwise, look for an approximate match of the adapter, less than
+        # max_substitutions different.
+        if has_near_match_substitutions_ngrams(adapter, self.sequence_line,
+                                               max_substitutions):
+            # set the adapter presence to True (and stop the function here)
+            self.adapter_present = True
+            return
+        # If no substring of sequence_line approximately matches the adapter,
+        # the function will eventually leave the loop above. The simple fact of
+        # arriving here proves that no match was found, therefore leave the
+        # adapter preence to False and simply leave the function.
 
     def __str__(self):
         """This function defines the string representation displayed when calling the print function on a Read object"""
